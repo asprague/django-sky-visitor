ee9ae06d7ac2943dc7ffe22c735c89a2e841f95c
Jeremy Blanchard <auzigog@gmail.com>
2012-03-21 17:49:20 -0700

    Fix default user/pass

--- example_project/example_project/tests.py
+++ example_project/example_project/tests.py
@@ -5,9 +5,9 @@ from django.core import mail
 from django.core.urlresolvers import reverse
 from django.test import TestCase
 
-ADMIN_EMAIL = 'admin@example.com'
-ADMIN_PASS = 'admin'
-ADMIN_RESET_URL = 'http://testserver/user/forgot_password/1-35t-d4e092280eb134000672/'
+USER_EMAIL = 'user@example.com'
+USER_PASS = 'adminadmin'
+USER_RESET_URL = 'http://testserver/user/forgot_password/1-35t-d4e092280eb134000672/'
 
 class BaseTestCase(TestCase):
     pass
@@ -19,8 +19,6 @@ class BaseTestCase(TestCase):
 
 class TestEmailLoginForm(BaseTestCase):
 
-    # TODO TEST: Login process works
-
     def test_login_form_should_be_email_based(self):
         response = self.client.get('/user/login/')
         self.assertEqual(response.status_code, 200)
@@ -28,26 +26,27 @@ class TestEmailLoginForm(BaseTestCase):
         self.assertIsInstance(form, EmailLoginForm)
 
 
+
 class TestForgotPasswordProcess(BaseTestCase):
 
     # TODO TEST: If an invalid email is entered into the forgot password form
     # TODO TEST: Token should be invalid after it is used once
+    # TODO TEST: token older than X weeks (will require removing hard coded reset URL=
 
     def test_forgot_password_form_should_send_email(self):
         response = self.client.get('/user/forgot_password/')
         self.assertEqual(response.status_code, 200)
 
-        data = {'email': ADMIN_EMAIL}
+        data = {'email': USER_EMAIL}
         response = self.client.post('/user/forgot_password/', data, follow=True)
-        self.assertEqual(response.status_code, 200)
         self.assertRedirects(response, '/user/forgot_password/check_email/')
         self.assertEqual(len(mail.outbox), 1)
         message = mail.outbox[0]
         self.assertEqual(message.subject, 'Password reset for testserver')
-        self.assertIn(ADMIN_RESET_URL, message.body)
+        self.assertIn(USER_RESET_URL, message.body)
 
     def test_reset_password_form_should_success_with_valid_input(self):
-        response = self.client.get(ADMIN_RESET_URL)
+        response = self.client.get(USER_RESET_URL)
         self.assertEqual(response.status_code, 200)
 
         new_pass = 'asdfasdf'
@@ -55,11 +54,11 @@ class TestForgotPasswordProcess(BaseTestCase):
             'new_password1': new_pass,
             'new_password2': new_pass,
         }
-        response = self.client.post(ADMIN_RESET_URL, data)
+        response = self.client.post(USER_RESET_URL, data)
         # Should redirect to '/'
         self.assertEqual(response.status_code, 302)
         self.assertEqual(response._headers['location'][1], 'http://testserver/')
-        user = AuthUser.objects.get(email=ADMIN_EMAIL)
+        user = AuthUser.objects.get(email=USER_EMAIL)
         # Should have a new password
         self.assertTrue(user.check_password(new_pass))
         # Should automatically log the user in
@@ -68,7 +67,7 @@ class TestForgotPasswordProcess(BaseTestCase):
 
     def test_reset_password_form_should_fail_with_invalid_token(self):
         # Should work fine for normal URL
-        response = self.client.get(ADMIN_RESET_URL)
+        response = self.client.get(USER_RESET_URL)
         self.assertEqual(response.status_code, 200)
         # User ID of this token is modified
         response = self.client.get('http://testserver/user/forgot_password/2-35t-d4e092280eb134000672/', follow=True)
